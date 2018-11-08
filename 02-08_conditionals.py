2de20c7f6d17ba83936d59675769bd77a3f355eb
polimacs <stanislav.polimac@gmail.com>
2018-11-08 16:29:40 +0100

    Renamed second lession file.

---
+++ 02-08_conditionals.py
@@ -0,0 +1,13 @@
+a = 5
+
+if a < 6
+    print('a is less than 6!')
+
+a = 5
+
+if a < 5:
+    print('manje od 5')
+elif 5 < a <= 10:
+    print('između 5 i 10')
+else:
+    print('više od 10')
--- 02-09_conditionals.py
+++
@@ -1,6 +0,0 @@
-a = 5
-
-if a < 6
-    print('a is less than 6!')
-
-
