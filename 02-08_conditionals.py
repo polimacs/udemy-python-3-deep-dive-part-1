dfd0354b0c0504b69839830ab4534520f7489a90
polimacs <stanislav.polimac@gmail.com>
2018-11-08 18:01:26 +0100

    Druga lekcija... ...gotova!

--- 02-08_conditionals.py
+++ 02-08_conditionals.py
@@ -1,6 +1,6 @@
 a = 5
 
-if a < 6
+if a < 6:
     print('a is less than 6!')
 
 a = 5
@@ -11,3 +11,8 @@ elif 5 < a <= 10:
     print('između 5 i 10')
 else:
     print('više od 10')
+
+#  conditional expression (ternary operator):
+a = 11
+b = 'veće od 5' if a > 5 else 'nije veće od 5'
+print(b)
